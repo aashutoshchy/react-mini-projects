51cc327e2804666bdcc6531e8f2bec35fa419299
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-09-06 23:25:21 +0545

    feat: "set up openWeather API"

---
+++ 10-weather-app/src/api/openWeather.js
@@ -0,0 +1,17 @@
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+export const getWeatherByCity = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const getNextHoursForecast = async (lat, lon) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
+  );
+  const data = await response.json();
+  return data;
+};
